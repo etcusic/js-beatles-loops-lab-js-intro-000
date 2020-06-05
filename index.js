a4bafefa1b1131022de4b1032df4669d116c3694
Eli Cusic <tntitans31@gmail.com>
2020-06-05 00:49:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // add solution here
-var theBeatlesPlay = function(){
-  var instruments = ["John Lennon plays Guitar",
-         "Paul McCartney plays Bass Guitar",
-         "George Harrison plays Lead Guitar",
-         "Ringo Starr plays Drums"]
-  return instruments
+var theBeatlesPlay = function(musicians, instruments){
+    var array = []
+    for (var i = 0; i < musicians.length; i++) {
+      array.push(musicians[i] + " plays " + instruments[i])
+    }
+    return array
 }
+// var instruments = ["John Lennon plays Guitar",
+//        "Paul McCartney plays Bass Guitar",
+//        "George Harrison plays Lead Guitar",
+//        "Ringo Starr plays Drums"]
+// return instruments
